e0be2b3e71be7c453123f1556c17c6eb86e0670e
utazaev <utazaev@gmail.com>
2025-02-24 21:07:10 +0300

    Добавление первого коммита

--- src/main/java/com/javarush/telegram/TinderBoltApp.java
+++ src/main/java/com/javarush/telegram/TinderBoltApp.java
@@ -13,9 +13,11 @@ import java.util.ArrayList;
 
 public class TinderBoltApp extends MultiSessionTelegramBot {
     public static final String TELEGRAM_BOT_NAME = "ut08bot"; //TODO: добавь имя бота в кавычках
-    public static final String TELEGRAM_BOT_TOKEN = "token"; //TODO: добавь токен бота в кавычках
-    public static final String OPEN_AI_TOKEN = "chat-gpt-token"; //TODO: добавь токен ChatGPT в кавычках
+    public static final String TELEGRAM_BOT_TOKEN = "telegram-token"; //TODO: добавь токен бота в кавычках
+    public static final String OPEN_AI_TOKEN = "ChatGPT-token"; //TODO: добавь токен ChatGPT в кавычках
 
+    private ChatGPTService chatGPT = new ChatGPTService(OPEN_AI_TOKEN);
+    private DialogMode currentMode = null;
     public TinderBoltApp() {
         super(TELEGRAM_BOT_NAME, TELEGRAM_BOT_TOKEN);
     }
@@ -27,9 +29,34 @@ public class TinderBoltApp extends MultiSessionTelegramBot {
 
 
         if (message.equals("/start")) {
+            currentMode = DialogMode.MAIN;
             sendPhotoMessage("main");
             String text = loadMessage("main");
             sendTextMessage(text);
+
+            showMainMenu(
+                    " главное меню бота", "/start",
+                    "генерация Tinder-профля \uD83D\uDE0E", "/profile",
+                    "сообщение для знакомства \uD83E\uDD70", "/opener",
+                    "переписка от вашего имени \uD83D\uDE08", "/message",
+                    "переписка со звездами \uD83D\uDD25", "/date",
+                    " задать вопрос чату GPT \uD83E\uDDE0", "/gpt"
+            );
+            return;
+        }
+
+        if (message.equals("/gpt")) {
+            currentMode = DialogMode.GPT;
+            sendPhotoMessage("gpt");
+            String text = loadMessage("gpt");
+            sendTextMessage(text);
+            return;
+
+        }
+        if (currentMode == DialogMode.GPT) {
+            String prompt = loadPrompt("gpt");
+            String answer = chatGPT.sendMessage ( prompt, message);
+            sendTextMessage(answer);
             return;
         }
 
